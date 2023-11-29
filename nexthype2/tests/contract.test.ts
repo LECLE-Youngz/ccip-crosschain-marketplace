ede6bb76a36a980042313c7a785ac73b7f97101d
TranNhi27 <18521208@gm.uit.edu.vn>
2023-11-29 21:15:05 +0700

    Initialize subgraph

---
+++ nexthype2/tests/contract.test.ts
@@ -0,0 +1,58 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { ERC721TokenCreated } from "../generated/schema"
+import { ERC721TokenCreated as ERC721TokenCreatedEvent } from "../generated/Contract/Contract"
+import { handleERC721TokenCreated } from "../src/contract"
+import { createERC721TokenCreatedEvent } from "./contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
+    let tokenAddress = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newERC721TokenCreatedEvent = createERC721TokenCreatedEvent(
+      owner,
+      tokenAddress
+    )
+    handleERC721TokenCreated(newERC721TokenCreatedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ERC721TokenCreated created and stored", () => {
+    assert.entityCount("ERC721TokenCreated", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ERC721TokenCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "owner",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ERC721TokenCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "tokenAddress",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
